22a89ce951e96262a434adfc259e6657a31dcc34
NuloSois <104397117+NuloSois@users.noreply.github.com>
2022-04-25 19:38:40 -0300

    Add files via upload

---
+++ teste/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Hoje iremos fazer aritmética");
+Console.WriteLine("Primeiro faremos a adição 4 + 4");
+var v = Console.ReadLine();
+Console.WriteLine(4 + 4);
+Console.WriteLine("Daí, obtemos 8, podemos chegar e fazer 8-6");
+var w = Console.ReadLine();
+Console.WriteLine(8 - 6);
+Console.WriteLine("Agora, iremos realizar a multiplicação, pegue o 2 que obtemos e multiplique por 5");
+var x = Console.ReadLine();
+Console.WriteLine(2 * 5);
+Console.WriteLine("Por último, vamos dividir o número obtido por 2");
+var z = Console.ReadLine();
+Console.WriteLine(10 / 2);
+
+
+
+
